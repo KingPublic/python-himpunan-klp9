7a9bab7f50c67fa6452eebd57111ae0ebf1406bb
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2024-11-18 19:51:15 +0800

    Uptodate

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-himpunan',  # Nama package
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[],  # Tidak menggunakan package tambahan
-    author='Nama Anda',
-    author_email='emailanda@example.com',
+    author='Andreyhs',
+    author_email='ahartawan01@gmail.com',
     description='Library Himpunan sederhana menggunakan Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/username/python-himpunan',  # URL repositori GitHub
+    url='https://github.com/KingPublic/python-himpunan-klp9/tree/main',  # URL repositori GitHub
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -18,4 +18,3 @@ setup(
     ],
     python_requires='>=3.6',
 )
-    
